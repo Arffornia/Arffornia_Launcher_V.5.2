48a0cc5d65ed9ae8a1dc8a59d470501f5f2b6995
The_Gost_sniper <54660359+TheGostsniperfr@users.noreply.github.com>
2024-08-06 16:08:51 +0200

    🔧 Fix podium ui + add switch btn

--- src/renderer/src/pages/Home.vue
+++ src/renderer/src/pages/Home.vue
@@ -1,65 +1,64 @@
 <template>
   <div class="notification-content">
-    <h2>Home page</h2>
-    <p>This is the home page content.</p>
+      <div class="podium">
+        <div class="content">
+          <div class="p2">
+            <p class="playerName">{{ bestPlayers ? bestPlayers[1]?.name : 'Chargement...' }}</p>
 
-    <div class="podium">
-      <div class="content">
-        <div class="p2">
-          <p class="playerName">{{ bestPlayers ? bestPlayers[1]?.name : 'Chargement...' }}</p>
+            <div class="player-skin">
+              <a :href="bestPlayers ? `/profile/${bestPlayers[1]?.name}` : '#'" title="Show profile">
+                <canvas class="skin_viewer" :data-username="bestPlayers ? bestPlayers[1]?.name : 'MHF_Steve'"></canvas>
+              </a>
+            </div>
 
-          <div class="player-skin">
-            <a :href="bestPlayers ? `/profile/${bestPlayers[1]?.name}` : '#'" title="Show profile">
-              <canvas class="skin_viewer" :data-username="bestPlayers ? bestPlayers[1]?.name : 'MHF_Steve'"></canvas>
-            </a>
+            <div class="shape">
+              <div class="textContainer">
+                <div class="rank">#2</div>
+                <div class="score">{{ bestPlayers ? `${bestPlayers[1]?.vote_count} ${scoreboardUnit}` : '...' }}</div>
+              </div>
+            </div>
           </div>
 
-          <div class="shape">
-            <div class="textContainer">
-              <div class="rank">#2</div>
-              <div class="score">{{ bestPlayers ? `${bestPlayers[1]?.vote_count} points` : '...' }}</div>
+          <div class="p1">
+            <p class="playerName">{{ bestPlayers ? bestPlayers[0]?.name : 'Chargement...' }}</p>
+
+            <div class="player-skin">
+              <a :href="bestPlayers ? `/profile/${bestPlayers[0]?.name}` : '#'" title="Show profile">
+                <canvas class="skin_viewer" :data-username="bestPlayers ? bestPlayers[0]?.name : 'MHF_Steve'"></canvas>
+              </a>
+            </div>
+
+            <div class="shape">
+              <div class="textContainer">
+                <div class="rank">#1</div>
+                <div class="score">{{ bestPlayers ? `${bestPlayers[0]?.vote_count} ${scoreboardUnit}` : '...' }}</div>
+              </div>
+            </div>
+          </div>
+
+          <div class="p3">
+            <p class="playerName">{{ bestPlayers ? bestPlayers[2]?.name : 'Chargement...' }}</p>
+
+            <div class="player-skin">
+              <a :href="bestPlayers ? `/profile/${bestPlayers[2]?.name}` : '#'" title="Show profile">
+                <canvas class="skin_viewer" :data-username="bestPlayers ? bestPlayers[2]?.name : 'MHF_Steve'"></canvas>
+              </a>
+            </div>
+
+            <div class="shape">
+              <div class="textContainer">
+                <div class="rank">#3</div>
+                <div class="score">{{ bestPlayers ? `${bestPlayers[2]?.vote_count} ${scoreboardUnit}` : '...' }}</div>
+              </div>
             </div>
           </div>
         </div>
-
-        <div class="p1">
-          <p class="playerName">{{ bestPlayers ? bestPlayers[0]?.name : 'Chargement...' }}</p>
-
-          <div class="player-skin">
-            <a :href="bestPlayers ? `/profile/${bestPlayers[0]?.name}` : '#'" title="Show profile">
-              <canvas class="skin_viewer" :data-username="bestPlayers ? bestPlayers[0]?.name : 'MHF_Steve'"></canvas>
-            </a>
-          </div>
-
-          <div class="shape">
-            <div class="textContainer">
-              <div class="rank">#1</div>
-              <div class="score">{{ bestPlayers ? `${bestPlayers[0]?.vote_count} points` : '...' }}</div>
-            </div>
-          </div>
-        </div>
-
-        <div class="p3">
-          <p class="playerName">{{ bestPlayers ? bestPlayers[2]?.name : 'Chargement...' }}</p>
-
-          <div class="player-skin">
-            <a :href="bestPlayers ? `/profile/${bestPlayers[2]?.name}` : '#'" title="Show profile">
-              <canvas class="skin_viewer" :data-username="bestPlayers ? bestPlayers[2]?.name : 'MHF_Steve'"></canvas>
-            </a>
-          </div>
-
-          <div class="shape">
-            <div class="textContainer">
-              <div class="rank">#3</div>
-              <div class="score">{{ bestPlayers ? `${bestPlayers[2]?.vote_count} points` : '...' }}</div>
-            </div>
-          </div>
+        <div class="title" @click="scoreboardTitleSwitcher">
+          <button class="titleBtn">
+            <p>{{ scoreboardTitle }}</p>
+          </button>
         </div>
       </div>
-      <div class="title">
-        <p>Meilleurs joueurs</p>
-      </div>
-    </div>
   </div>
 </template>
 
@@ -70,6 +69,8 @@ import { fetchBestPlayersByVote } from '../js/arfforniaApi.js';
 
 const bestPlayers = ref(null);
 const error = ref(null);
+const scoreboardUnit = ref("votes");
+const scoreboardTitle = ref("Best voters");
 
 onMounted(async () => {
   initializeSkinViewers();
@@ -80,63 +81,84 @@ onMounted(async () => {
     console.error("Failed to fetch best players:", err);
   }
 });
+
+function scoreboardTitleSwitcher(event) {
+  if(scoreboardTitle == "Best voters") {
+    scoreboardTitle = "Best Players";
+    scoreboardUnit = "points";
+  } else {
+    scoreboardTitle = "Best voters";
+    scoreboardUnit = "votes";
+  }
+}
 </script>
 
 
 <style scoped>
+  .podium {
+    transform: scale(0.7);
+    transform-origin: top left;
+  }
 
   .podium .content {
-
     display: flex;
     justify-content: center;
     gap: 6%;
   }
 
   .podium .title {
-    padding: 15px;
-    width: 57%;
+    width: 80%;
     margin-left: auto;
     margin-right: auto;
     margin-top: 1.5%;
-
     text-align: center;
     color: #fff;
+    background-color: #2C3239;
+    border: solid 3px #ff7300;
     border-radius: 10px;
+
+  }
+
+  .podium .titleBtn {
+    background-color: #00000000;
+    text-align: center;
+    color: #fff;
     font-size: 250%;
     font-weight: 700;
-    background-color: #2C3239;
-
-    border: solid 3px #ff7300;
+    white-space: nowrap;
+    border: none;
   }
 
   .p2, .p1, .p3 {
     align-self: flex-end;
-    width: 15%;
-
+    width: 20%;
   }
 
   .podium .shape {
     background-color: #2C3239;
     border-radius: 10px;
     border: solid 3px #ff7300;
-
-  }
-
-  .podium .shape {
     position: relative;
-    padding: 15px;
+    padding: 40px 50px;
   }
 
   .p2 .shape {
-    padding-top: 55%;
+    padding-top: 35%;
   }
 
   .p1 .shape {
-    padding-top: 75%;
+    padding-top: 55%;
   }
 
   .p3 .shape {
-    padding-top: 40%;
+    padding-top: 20%;
+  }
+
+  .podium .player-skin {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 10px;
   }
 
   .podium .textContainer {
@@ -155,24 +177,26 @@ onMounted(async () => {
 
   .podium .playerName {
     font-weight: 700;
-    font-size: 150%;
+    font-size: 60%;
     color: #d4af37;
     text-align: center;
+    transform: scale(2);
+
   }
 
   .podium .p2 .rank {
+    font-weight: 600;
+    font-size: 225%;
+  }
+
+  .podium .p1 .rank {
     font-weight: 650;
     font-size: 300%;
   }
 
-  .podium .p1 .rank {
-    font-weight: 700;
-    font-size: 375%;
-  }
-
   .podium .p3 .rank {
-    font-weight: 600;
-    font-size: 225%;
+    font-weight: 550;
+    font-size: 175%;
   }
 
   .podium .score {
