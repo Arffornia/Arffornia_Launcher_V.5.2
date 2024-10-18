8e99c765c653e5559e39d7efb8a9b754481605a4
brian.perret <brian.perret@epita.fr>
2024-10-18 10:33:49 +0200

    Start shop

---
+++ src/renderer/src/components/Shop.vue
@@ -0,0 +1,42 @@
+<template>
+<div class="shop-container">
+  <p id="shop-title"></p>
+
+  <div class="item-container">
+    <div class="item-title"</div>
+    <div class="item-description"></div>
+  </div>
+</div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+#title {
+  font-size: 300%;
+  font-weight: 500;
+
+  border-left: 4px solid #ff7300;
+  border-top-left-radius: 3px;
+  border-bottom-left-radius: 3px;
+}
+
+.item-title {
+  font-size: 200%;
+  font-weight: 500;
+}
+
+.item-description {
+  font-size: 110%;
+  font-weight: 400;
+}
+
+.item-container {
+  color: #fff;
+  border: solid 3px #ff7300;
+  padding: 20px;
+  backdrop-filter: blur(4px);
+}
+</style>
